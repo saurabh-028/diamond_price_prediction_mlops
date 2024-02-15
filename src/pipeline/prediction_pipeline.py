f981800fb8b8f3e46feb48ae3caeb319be3a5ad1
saurabh <saurabhshinde028@gmail.com>
2024-02-15 19:20:58 +0530

    App created

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -11,8 +11,8 @@ class PredictPipeline:
 
     def predict(self,data):
         try:
-            preprocessor_path = os.path.join("artifatct", "preprocessor.pkl")
-            model_path = os.path.join("artifatct", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+            model_path = os.path.join("artifacts", "model.pkl")
 
             preprocessor = load_object(preprocessor_path)
             model = load_object(model_path)
@@ -25,8 +25,44 @@ class PredictPipeline:
         except Exception as e:
             raise customexception(e,sys)
         
-class MakeDataframe:
-    def __init__(self) -> None:
-        pass
+class CustomeData:
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+            
     def get_data_as_dataframe(self):
-        pass
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+                }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df   
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise customexception(e,sys)
